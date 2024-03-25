42eefdf13ea7e2e15a02c77ec508ce48a47dcbe4
Sarthak Nande <94531691+sarthu210@users.noreply.github.com>
2024-03-25 18:46:20 +0530

    Create postgresSql.js

---
+++ postgresSql.js
@@ -0,0 +1,11 @@
+import pg from "pg";
+
+const db = new Client({
+  host: 'my.database-server.com',
+  port: 5334,
+  database: 'database-name',
+  user: 'database-user',
+  password: 'secretpassword!!',
+})
+
+await db.connect();
